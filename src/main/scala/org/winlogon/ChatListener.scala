64eefdd28af0fd2573d2f357818f0e762cd458cf
winlogon <walker84837@gmail.com>
2024-12-25 19:09:32 +0100

    feat: add initial code for text colors in terminal

--- src/main/scala/org/winlogon/ChatListener.scala
+++ src/main/scala/org/winlogon/ChatListener.scala
@@ -4,10 +4,13 @@ import org.bukkit.{Bukkit, ChatColor}
 import org.bukkit.entity.Player
 import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.plugin.Plugin
+import org.fusesource.jansi.{Ansi, AnsiConsole}
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
-import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+import io.papermc.paper.event.player.AsyncChatEvent
+
+import scala.util.matching.Regex
 
 case class Placeholders(
   prefix: String,
@@ -27,7 +30,7 @@ class ChatListener(plugin: Plugin) extends Listener {
     val replacements = Placeholders(
       prefix = getPrefix(player),
       suffix = getSuffix(player),
-      username = player.getName, // Added the missing username argument
+      username = player.getName,
       world = player.getWorld.getName,
       message = formatMessage(player, message)
     )
@@ -42,9 +45,26 @@ class ChatListener(plugin: Plugin) extends Listener {
       .replace("$message", replacements.message)
       .replace("$world", replacements.world)
 
+    // Log with Jansi for terminal colors
+    // TODO: fix white not being shown properly and original message showing up in console
+    AnsiConsole.systemInstall()
+    val ansiPattern: Regex = "&([0-9a-f])".r
+    val terminalMessage = ansiPattern.replaceAllIn(chatFormat, m => {
+      val colorCode = Integer.parseInt(m.group(1), 16)
+      Ansi.ansi().fg(Ansi.Color.values()(colorCode % Ansi.Color.values().length)).toString
+    }).replaceAll("&r", Ansi.ansi().reset().toString)
+       // Strip unsupported formatting as we have no way to show obfuscated
+       // text in the terminal
+       .replaceAll("&([k-or])", "")
+
+    // Ensure reset at the end or else I get artefacts of this
+    Bukkit.getConsoleSender.sendMessage(terminalMessage + Ansi.ansi().reset().toString)
+    AnsiConsole.systemUninstall()
+
+    // Translate Minecraft color codes for in-game chat
     val msg = ChatColor.translateAlternateColorCodes('&', chatFormat)
     // Use Adventure API to create a Component for the renderer
-    val chatComponent = Component.text(msg).color(NamedTextColor.WHITE)
+    val chatComponent = Component.text(msg)
     event.renderer((_, _, _, _) => chatComponent)
   }
 }
