8e8475e313f543d87a2f68a121cf7e5295260f3b
winlogon <walker84837@gmail.com>
2025-01-11 22:18:19 +0100

    fix(ansi): make chat colors consistent
    Fixes #1

--- src/main/scala/org/winlogon/ChatListener.scala
+++ src/main/scala/org/winlogon/ChatListener.scala
@@ -2,11 +2,12 @@ package org.winlogon
 
 import org.bukkit.{Bukkit, ChatColor}
 import org.bukkit.entity.Player
-import org.bukkit.event.{EventHandler, Listener}
+import org.bukkit.event.{EventHandler, Listener, EventPriority}
 import org.bukkit.plugin.Plugin
 import org.fusesource.jansi.{Ansi, AnsiConsole}
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import io.papermc.paper.event.player.AsyncChatEvent
 
@@ -21,10 +22,28 @@ case class Placeholders(
 )
 
 class ChatListener(plugin: Plugin) extends Listener {
-  @EventHandler
+  val colorMap: Map[Char, Ansi.Color] = Map(
+    '0' -> Ansi.Color.BLACK,
+    '1' -> Ansi.Color.BLUE,
+    '2' -> Ansi.Color.GREEN,
+    '3' -> Ansi.Color.CYAN,
+    '4' -> Ansi.Color.RED,
+    '5' -> Ansi.Color.MAGENTA,
+    '6' -> Ansi.Color.YELLOW,
+    '7' -> Ansi.Color.WHITE,
+    '8' -> Ansi.Color.BLACK,
+    '9' -> Ansi.Color.BLUE,
+    'a' -> Ansi.Color.GREEN,
+    'b' -> Ansi.Color.CYAN,
+    'c' -> Ansi.Color.RED,
+    'd' -> Ansi.Color.MAGENTA,
+    'e' -> Ansi.Color.YELLOW,
+    'f' -> Ansi.Color.WHITE
+  )
+
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
   def onPlayerChat(event: AsyncChatEvent): Unit = {
     val player: Player = event.getPlayer
-    // Extract the plain text of the message using PlainTextComponentSerializer
     val message = PlainTextComponentSerializer.plainText().serialize(event.message())
 
     val replacements = Placeholders(
@@ -35,9 +54,7 @@ class ChatListener(plugin: Plugin) extends Listener {
       message = formatMessage(player, message)
     )
 
-    // Retrieve chat format from config
     var chatFormat = plugin.getConfig.getString("chat.format", "&7$prefix$username$suffix&7 » &f$message")
-    // Translate placeholders
     chatFormat = chatFormat
       .replace("$prefix", replacements.prefix)
       .replace("$suffix", replacements.suffix)
@@ -45,26 +62,28 @@ class ChatListener(plugin: Plugin) extends Listener {
       .replace("$message", replacements.message)
       .replace("$world", replacements.world)
 
-    // Log with Jansi for terminal colors
-    // TODO: fix white not being shown properly and original message showing up in console
     AnsiConsole.systemInstall()
+
+    val resetCode = Ansi.ansi().reset().toString
     val ansiPattern: Regex = "&([0-9a-f])".r
     val terminalMessage = ansiPattern.replaceAllIn(chatFormat, m => {
-      val colorCode = Integer.parseInt(m.group(1), 16)
-      Ansi.ansi().fg(Ansi.Color.values()(colorCode % Ansi.Color.values().length)).toString
-    }).replaceAll("&r", Ansi.ansi().reset().toString)
-       // Strip unsupported formatting as we have no way to show obfuscated
-       // text in the terminal
-       .replaceAll("&([k-or])", "")
+      val colorCode = m.group(1).charAt(0)
+      val ansiColor = colorMap.getOrElse(colorCode, Ansi.Color.DEFAULT)
+      Ansi.ansi().fg(ansiColor).toString
+    }).replaceAll("&r", resetCode)
+      .replaceAll("&([k-or])", "")
 
-    // Ensure reset at the end or else I get artefacts of this
-    Bukkit.getConsoleSender.sendMessage(terminalMessage + Ansi.ansi().reset().toString)
+    Bukkit.getConsoleSender.sendMessage(terminalMessage + resetCode)
     AnsiConsole.systemUninstall()
 
-    // Translate Minecraft color codes for in-game chat
-    val msg = ChatColor.translateAlternateColorCodes('&', chatFormat)
-    // Use Adventure API to create a Component for the renderer
-    val chatComponent = Component.text(msg)
-    event.renderer((_, _, _, _) => chatComponent)
+    val hexPattern: Regex = "<#[0-9a-fA-F]{6}>".r
+    val coloredMessage = hexPattern.replaceAllIn(chatFormat, m => {
+      val hexColor = m.matched.substring(2, 8)
+      TextColor.fromHexString("#" + hexColor).asHexString()
+    })
+
+    val componentMessage = LegacyComponentSerializer.legacyAmpersand().deserialize(coloredMessage)
+
+    event.renderer((_, _, _, _) => componentMessage)
   }
 }
