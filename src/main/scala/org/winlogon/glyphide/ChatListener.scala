2f12b6c8cfd8be6212f1b01b7c82780d34f35853
winlogon <walker84837@gmail.com>
2025-04-18 17:36:48 +0200

    refactor: rework chat utilities and add givens
    This change consolidates message formatting logic and player
    metadata access into a new `Formatter` object. By using Scala 3
    givens and extension methods, the code is now more modular and
    reusable, especially for resolving MiniMessage tags. The old
    duplicate logic for formatting messages and resolving user prefixes
    and suffixes has been removed or simplified.
    
    Summary of changes:
    
    - Move chat formatting and LuckPerms user logic into Formatter
    - Replace duplicated tag resolver definitions with givens
    - Simplify `formatMessage`, `getPrefix`, and `getSuffix` functions
    - Add `convertLegacyToMiniMessage` helper to Formatter
    - Apply SPDX license headers to relevant files
    - Improve player permission checks using enum and extension

--- src/main/scala/org/winlogon/glyphide/ChatListener.scala
+++ src/main/scala/org/winlogon/glyphide/ChatListener.scala
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MPL-2.0
 package org.winlogon.glyphide
 
 import org.bukkit.{Bukkit, ChatColor}
@@ -7,29 +8,22 @@ import org.bukkit.plugin.Plugin
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
-import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 
 import io.papermc.paper.event.player.AsyncChatEvent
 
-import scala.util.matching.Regex
-
-case class Placeholders(
-    prefix: Component,
-    suffix: Component,
-    username: String,
-    world: String
-)
+import Formatter.{given, *}
 
 class ChatListener(plugin: Plugin) extends Listener {
-    val miniMessage = MiniMessage.miniMessage()
+    private val miniMessage = MiniMessage.miniMessage()
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     def onPlayerChat(event: AsyncChatEvent): Unit = {
-        val player: Player = event.getPlayer
+        val player = event.getPlayer
         val message = PlainTextComponentSerializer.plainText().serialize(event.message())
+
+        given TagResolver = Formatter.basicResolver
 
         val replacements = Placeholders(
             prefix = getPrefix(player),
@@ -48,18 +42,19 @@ class ChatListener(plugin: Plugin) extends Listener {
         )
         val formattedMessageComponent = formatMessage(player, message)
 
-        val component: Component = miniMessage
-            .deserialize(chatFormat, advancedTagsResolver)
-            .replaceText(builder =>
-                builder.matchLiteral("$prefix").replacement(replacements.prefix)
-            )
-            .replaceText(builder =>
-                builder.matchLiteral("$suffix").replacement(replacements.suffix)
-            )
-            .replaceText(builder =>
-                builder.matchLiteral("$message").replacement(formattedMessageComponent)
-            )
+        val component = miniMessage
+            .deserialize(chatFormat, summon[TagResolver])
+            .replaceText(builder => builder.matchLiteral("$prefix").replacement(replacements.prefix))
+            .replaceText(builder => builder.matchLiteral("$suffix").replacement(replacements.suffix))
+            .replaceText(builder => builder.matchLiteral("$message").replacement(formattedMessageComponent))
 
         event.renderer((source, _, _, _) => component)
     }
 }
+
+case class Placeholders(
+    prefix: Component,
+    suffix: Component,
+    username: String,
+    world: String
+)
--- src/main/scala/org/winlogon/glyphide/Utilities.scala
+++ src/main/scala/org/winlogon/glyphide/Utilities.scala
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MPL-2.0
 package org.winlogon.glyphide
 
 import org.bukkit.ChatColor
@@ -13,100 +14,87 @@ import net.luckperms.api.{LuckPerms, LuckPermsProvider}
 
 import scala.util.{Try, Success, Failure}
 
-val advancedTagsResolver = TagResolver
-    .builder()
-    .resolver(StandardTags.color())
-    .resolver(StandardTags.decorations())
-    .resolver(StandardTags.gradient())
-    .resolver(StandardTags.rainbow())
-    .resolver(StandardTags.clickEvent())
-    .resolver(StandardTags.hoverEvent())
-    .resolver(StandardTags.transition())
-    .build()
+object Formatter {
+    enum Permission(val name: String) {
+        case Admin extends Permission("glyphide.admin")
+    }
 
-val basicTagsResolver = TagResolver
-    .builder()
-    .resolver(StandardTags.color())
-    .resolver(StandardTags.decorations())
-    .resolver(StandardTags.gradient())
-    .build()
+    private val basicTagsResolver = TagResolver.builder()
+        .resolver(StandardTags.color())
+        .resolver(StandardTags.decorations())
+        .resolver(StandardTags.gradient())
+        .build()
 
-/** Get the prefix of a player.
-  *
-  * @param player The player to get the prefix of
-  * @return The player's prefix
-  */
-def getPrefix(player: Player): Component = {
-    Option(LuckPermsProvider.get().getUserManager.getUser(player.getUniqueId))
-        .flatMap(user => Option(user.getCachedData.getMetaData.getPrefix))
-        .map(prefix =>
-            MiniMessage
-                .miniMessage()
-                .deserialize(convertLegacyToMiniMessage(prefix), basicTagsResolver)
-        )
-        .getOrElse(Component.empty())
-}
+    private val advancedTagsResolver = TagResolver.builder()
+        .resolver(StandardTags.color())
+        .resolver(StandardTags.decorations())
+        .resolver(StandardTags.gradient())
+        .resolver(StandardTags.rainbow())
+        .resolver(StandardTags.clickEvent())
+        .resolver(StandardTags.hoverEvent())
+        .resolver(StandardTags.transition())
+        .build()
 
-/** Get the suffix of a player.
-  * @param player The player to get the suffix of
-  * @return The player's suffix
-  */
-def getSuffix(player: Player): Component = {
-    Option(LuckPermsProvider.get().getUserManager.getUser(player.getUniqueId))
-        .flatMap(user => Option(user.getCachedData.getMetaData.getSuffix))
-        .map(suffix =>
-            MiniMessage
-                .miniMessage()
-                .deserialize(convertLegacyToMiniMessage(suffix), basicTagsResolver)
-        )
-        .getOrElse(Component.empty())
-}
+    given basicResolver: TagResolver = basicTagsResolver
+    given advancedResolver: TagResolver = advancedTagsResolver
 
-/** Format a message for a player, based on their permissions.
-  *
-  * @param player The player to format the message for
-  * @param message The message to format
-  * @return The formatted message
-  * @see https://minecraft.fandom.com/wiki/Formatting_codes
-  */
-def formatMessage(player: Player, message: String): Component = {
-    if (player.hasPermission("glyphide.admin")) {
-        // TODO: find better way to escape MiniMessages
-        val convertedString = convertLegacyToMiniMessage(message)
+    extension (p: Player) {
+        private def luckPermsUser: Option[User] = Try(LuckPermsProvider.get())
+            .toOption
+            .flatMap(lp => Option(lp.getUserManager.getUser(p.getUniqueId)))
+    }
+
+    extension (user: User) {
+        private def prefix: Option[String] = Option(user.getCachedData.getMetaData.getPrefix)
+        private def suffix: Option[String] = Option(user.getCachedData.getMetaData.getSuffix)
+    }
+
+    private def deserializeComponent(input: String)(using resolver: TagResolver): Component = {
+        MiniMessage.miniMessage().deserialize(convertLegacyToMiniMessage(input), resolver)
+    }
+
+    private def getComponent(player: Player, extractor: User => Option[String])(using resolver: TagResolver): Component = {
+        player.luckPermsUser
+            .flatMap(extractor)
+            .map(deserializeComponent)
+            .getOrElse(Component.empty())
+    }
+
+    def getPrefix(player: Player)(using resolver: TagResolver): Component = 
+        getComponent(player, _.prefix)
+
+    def getSuffix(player: Player)(using resolver: TagResolver): Component = 
+        getComponent(player, _.suffix)
+
+    def formatMessage(player: Player, message: String): Component = {
+        player.hasPermission(Permission.Admin.name) match {
+            case true => usingAdvancedResolver(message)
+            case false => basicFormat(message)
+        }
+    }
+
+    private def usingAdvancedResolver(message: String): Component = {
+        given TagResolver = advancedResolver
+        val converted = convertLegacyToMiniMessage(message)
             .replaceAll("\\\\>", ">")
             .replaceAll("\\\\<", "<")
-        val mm: MiniMessage = MiniMessage.builder().tags(advancedTagsResolver).build()
-
-        Try {
-            mm.deserialize(convertedString, advancedTagsResolver)
-        } match {
-            case Success(parsedComponent) =>
-                parsedComponent
-            case Failure(e) =>
-                LegacyComponentSerializer.legacyAmpersand().deserialize(convertedString)
+        
+        Try(MiniMessage.builder().build().deserialize(converted, summon[TagResolver])) match {
+            case Success(c) => c
+            case Failure(_) => LegacyComponentSerializer.legacyAmpersand().deserialize(converted)
         }
-    } else {
-        LegacyComponentSerializer
-            .legacyAmpersand()
-            .deserialize(
-                message
-                    .replaceAll("(?i)&k", "")
-                    .replaceAll("(?i)&([0-9a-f])", "&$1")
-            )
     }
-}
 
-/** Converts a string with legacy ampersands to MiniMessage-formatted messages.
-  *
-  * @param input The legacy-formatted string
-  * @return The MiniMessage-formatted message
-  * @see https://docs.papermc.io/paper/dev/component-api/introduction#legacycomponentserializer
-  */
-def convertLegacyToMiniMessage(input: String): String = {
-    val s = ChatColor.translateAlternateColorCodes('&', input)
-    MiniMessage
-        .miniMessage()
-        .serialize(
-            LegacyComponentSerializer.legacySection().deserialize(s)
+    private def basicFormat(message: String): Component = {
+        LegacyComponentSerializer.legacyAmpersand().deserialize(
+            message
+                .replaceAll("(?i)&k", "")
+                .replaceAll("(?i)&([0-9a-f])", "&$1")
         )
+    }
+
+    def convertLegacyToMiniMessage(input: String): String = {
+        val translated = ChatColor.translateAlternateColorCodes('&', input)
+        MiniMessage.miniMessage().serialize(LegacyComponentSerializer.legacySection().deserialize(translated))
+    }
 }
