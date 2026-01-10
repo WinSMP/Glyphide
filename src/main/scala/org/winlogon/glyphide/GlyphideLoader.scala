7cb0786b1790ccc2b50624f7a672aa96cd219a78
winlogon <walker84837@gmail.com>
2026-01-10 17:16:02 +0100

    refactor(loader): make dependency declarations shorter
    This commit removes the dependency on the ScalaLoader plugin. This is
    because relying on a small plugin to include the scala-library.jar is
    overkill and should just be integrated with this plugin's loader
    instead.

--- src/main/scala/org/winlogon/glyphide/GlyphideLoader.scala
+++ src/main/scala/org/winlogon/glyphide/GlyphideLoader.scala
@@ -8,9 +8,14 @@ import org.eclipse.aether.artifact.DefaultArtifact
 import org.eclipse.aether.graph.Dependency
 import org.eclipse.aether.repository.RemoteRepository
 
+import java.util.List as JavaList
+
 class GlyphideLoader extends PluginLoader {
     override def classloader(classpathBuilder: PluginClasspathBuilder): Unit = {
         val resolver = MavenLibraryResolver();
+
+        // See https://mvnrepository.com/artifact/org.scala-lang/scala3-library
+        val scala = "3.7.4"
 
         resolver.addRepository(
             RemoteRepository.Builder(
@@ -28,20 +33,18 @@ class GlyphideLoader extends PluginLoader {
             ).build()
         );
 
-        resolver.addDependency(
-            Dependency(
-                DefaultArtifact("org.unbescape:unbescape:1.1.6.RELEASE"),
-                null
-            )
-        );
+        val dependencies = JavaList.of(
+            dependency("org.unbescape:unbescape:1.1.6.RELEASE"),
+            dependency("org.winlogon:retrohue:0.1.0"),
+            dependency("org.scala-lang:scala3-library_3:".concat(scala))
+        )
 
-        resolver.addDependency(
-            Dependency(
-                DefaultArtifact("org.winlogon:retrohue:0.1.0"),
-                null
-            )
-        );
+        dependencies.forEach((d: Dependency) => resolver.addDependency(d))
 
         classpathBuilder.addLibrary(resolver);
+    }
+
+    private def dependency(dep: String): Dependency = {
+        Dependency(DefaultArtifact(dep), null)
     }
 }
